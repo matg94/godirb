461b604f813023607b5cae4613f8dfda4fa1816a
mat <mat.gautron@gmail.com>
2022-10-07 05:39:44 +0100

    sets up the install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.godirb
 curl -s https://raw.githubusercontent.com/matg94/godirb/main/default.yaml > ~/.godirb/default.yaml
 curl -s https://raw.githubusercontent.com/matg94/godirb/main/common.txt > ~/.godirb/common.txt
-wget https://github.com/matg94/godirb/releases/download/initial_installable_release/godirb
+wget https://github.com/matg94/godirb/releases/download/v1.0/godirb
 
 chmod +x ./godirb
 mv ./godirb /usr/local/bin
